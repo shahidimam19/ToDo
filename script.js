66ab49d52fc7c8b7578413e7cf83ef2aadd9b468
Shahid Imam <shahidimam190202@gmail.com>
2025-08-11 22:42:48 +0530

    added register page

--- script.js
+++ script.js
@@ -2,52 +2,67 @@
 
 const loginForm = document.querySelector('.Login-form')
 const loginBtn = document.querySelector('#login-btn')
-let loginCont = document.querySelector('.login-container')
+let loginCont = document.querySelector('.signup')
 let loader = document.querySelector('.loader')
+let regLink = document.querySelector('.login p a')
+let loginLink = document.querySelector('.register p a')
+let loginPage = document.querySelector('.login')
+let regPage = document.querySelector('.register')
+
 
 const scriptURL = "https://script.google.com/macros/s/AKfycbxJtoI0R1CYK90sKCV252zuP9eZZJg5k1y1BT-nQZOAEP5SIFV3Uw-z7k1PaFiI43U0/exec"; // from Apps Script
 
-    // function registerUser() {
-    //   fetch(scriptURL, {
-    //     method: "POST",
-    //     body: JSON.stringify({
-    //       action: "register",
-    //       username: document.getElementById("regUser").value,
-    //       password: document.getElementById("regPass").value
-    //     })
-    //   })
-    //   .then(res => res.json())
-    //   .then(data => document.getElementById("message").innerText = data.status);
-    // }
+// function registerUser() {
+//   fetch(scriptURL, {
+//     method: "POST",
+//     body: JSON.stringify({
+//       action: "register",
+//       username: document.getElementById("regUser").value,
+//       password: document.getElementById("regPass").value
+//     })
+//   })
+//   .then(res => res.json())
+//   .then(data => document.getElementById("message").innerText = data.status);
+// }
 
-    function loginUser() {
-      fetch(scriptURL, {
-        method: "POST",
-        body: JSON.stringify({
-          action: "login",
-          username: document.getElementById("loginUser").value,
-          password: document.getElementById("loginPass").value
+function loginUser() {
+  fetch(scriptURL, {
+    method: "POST",
+    body: JSON.stringify({
+      action: "login",
+      username: document.getElementById("loginUser").value,
+      password: document.getElementById("loginPass").value
+    })
+  })
+    .then(res => res.json())
+    .then(data => {
+      console.log(data.message);
+      if (data.success) {
+        // alert("Logged In");
+        let userName = document.querySelectorAll('.username')
+        userName.forEach((user) => {
+          user.textContent = data.message
         })
-      })
-      .then(res => res.json())
-      .then(data => {
-        console.log(data.message);
-        if (data.success) {
-            // alert("Logged In");
-            let userName = document.querySelectorAll('.username')
-            userName.forEach((user)=>{
-                user.textContent= data.message
-            })
-            loginCont.classList.add('inactive')
-            loginBtn.textContent= 'Logout'
-        }
-        loader.style.display = 'none'
-      });
-    }
+        loginCont.classList.add('inactive')
+        loginBtn.textContent = 'Logout'
+      }
+      loader.style.display = 'none'
+    });
+}
 
-loginForm.addEventListener('submit', (e)=>{
-    loginCont.classList.add("blur-effect");
-    loader.style.display = 'block'
-    e.preventDefault()
-    loginUser()
+loginForm.addEventListener('submit', (e) => {
+  loginCont.classList.add("blur-effect");
+  loader.style.display = 'block'
+  e.preventDefault()
+  loginUser()
+})
+
+regLink.addEventListener('click', ()=>{
+loginPage.classList.add('hide')
+regPage.classList.add('show')
+})
+
+loginLink.addEventListener('click', ()=>{
+loginPage.classList.remove('hide')
+regPage.classList.remove('show')
 })
